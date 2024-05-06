224419a8b851ae92fcfaef6c274046d5d04582b4
Adebanjo Favour Mayowa <favouradebanjo2@gmail.com>
2024-05-06 14:34:00 +0100

    Working and learning Arraylist

---
+++ ArrayList.java
@@ -0,0 +1,16 @@
+
+/*
+ * Implementing an ArrayList in Java using dynamic arrays.
+ */
+import java.util.ArrayList
+
+public class ArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> name_of_students = new ArrayList<String>();
+        name_of_students.add("Vickydev")
+        name_of_students.add("JOshua")
+        name_of_students.add("Micheal")
+        name_of_students.add("Tolulope")
+        name_of_students.add("Victor")
+    }
+}
