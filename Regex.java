e1cf2e633f69706e5309d5a77119b99e675bd7c6
Adebanjo Favour Mayowa <favouradebanjo2@gmail.com>
2024-05-20 18:58:18 +0100

    Worked and trying to master regex in java sytanx

--- Regex.java
+++ Regex.java
@@ -1,3 +1,6 @@
+//  JavaRegularExpression   //  --- Abbreviate to "Reg_Expn"
+//  This is java code it is called java regular expression A.K.A(REGEX) in javaScript   //
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -5,9 +8,11 @@ public class Regex {
     String comment = "This is a java class";
 
     public static void main(String[] args) {
+        // Corrected to use java.util.regex.Pattern and java.util.regex.Matcher
         Pattern pat = Pattern.compile("class", Pattern.CASE_INSENSITIVE);
 
-        JavaRegularExpression instance = new JavaRegularExpression(); 
+        Reg_Expn instance = new Reg_Expn(); // Create an instance to access the non-static
+                                            // variable
         Matcher mat = pat.matcher(instance.comment);
         boolean find = mat.find();
 
@@ -17,4 +22,24 @@ public class Regex {
             System.out.println("No match found in the text");
         }
     }
+
+    // Regular Expression;
+    // Compile options;
+    // Pattern.CASE_INSENSITIVE;
+    // Pattern.LITERA;
+    // Pattern.UNICODE_CASE;
+
+    // Regular Expression Patterns
+    // [afx]
+    // [^afx]
+    // [0-9]
+
+    // Regular Expression Metacharacters
+    // | e.g [afx][abc]
+    // .
+    // ^ ^the
+    // $ class$
+    // \d
+    // \s
+    // \b
 }
